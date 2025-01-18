d0b9ac8953394167fcb77d5611e0919f2ab05a3d
Sco10969 <tommy2909@hotmail.fr>
2025-01-18 13:09:18 +0100

    EXO 9 - ✅

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,13 @@
 // votre code ici
 
 export default checkAge;
+
+function checkAge(age) {
+    if (age < 18) {
+        return "Vous êtes mineur.";
+    } else if (age >= 18 && age <= 65) {
+        return "Vous êtes majeur.";
+    } else {
+        return "Vous êtes senior.";
+    }
+}

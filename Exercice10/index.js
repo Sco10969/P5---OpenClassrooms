da77bdc22551700820d51d6fb41020a75e16d750
Sco10969 <tommy2909@hotmail.fr>
2025-01-18 13:22:48 +0100

    EXO 10 - ✅

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,14 @@
 //votre code ici
 
 export default printNumbers
+
+function printNumbers(n) {
+    let result = "";
+    for (let i = 1; i <= n; i++) {
+        result += i;
+        if (i < n) {
+            result += " ";
+        }
+    }
+    return result;
+}
